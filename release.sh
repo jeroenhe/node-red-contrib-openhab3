e68f4f226ef2ef8afd2aac5a901944527ef12723
Jeroen <gitlab.com@hendricksen.eu>
2021-12-09 09:14:20 +0100

    Enable auto release to github

--- release.sh
+++ release.sh
@@ -4,5 +4,9 @@
 # ToDo: test the new version in available from the README.md, ensuring
 # the change was documented.
 
-# Make sure the actual changes have been committed
+# Howto release a change to NPM:
+# 1. Make sure changes end up on the main branch on the server. Include an
+#    update to README.md as well.
+# 2. Run this script and choose the release # the same as done in README.md
+
 release-it --only-version
