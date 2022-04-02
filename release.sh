59be2a6384a868d374471008b4a8dc5160726245
Jeroen <gitlab.com@hendricksen.eu>
2022-04-02 10:30:55 +0200

    Improve release process

--- release.sh
+++ release.sh
@@ -4,6 +4,14 @@
 # ToDo: test the new version in available from the README.md, ensuring
 # the change was documented.
 
+# Test for any changes with regards to linting. If so, abort the release.
+npm run lint
+EXIT_CODE=$?
+if [ "${EXIT_CODE}" != "0" ]; then
+    echo "npm run lint returned errors. Try run \"npm run lint-fix\" and commit changes to git before re-running this script."
+    exit $EXIT_CODE
+fi
+
 # Howto release a change to NPM:
 # 1. Make sure changes end up on the main branch on the server. Include an
 #    update to README.md as well.
