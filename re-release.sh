73cd3497379d81fa9c3135570fdec551a352eb77
Jeroen <gitlab.com@hendricksen.eu>
2022-04-02 10:19:21 +0200

    add re-relase.sh

---
+++ re-release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Run this to release a patch version. Make sure the README.md is already upgraded
+# ToDo: test the new version in available from the README.md, ensuring
+# the change was documented.
+
+# Howto release a change to NPM:
+# 1. Make sure changes end up on the main branch on the server. Include an
+#    update to README.md as well.
+# 2. Run this script and choose the release # the same as done in README.md
+
+release-it --no-increment $@
