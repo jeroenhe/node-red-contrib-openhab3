80520d131796e5867fd0781732f7efdd246c9bda
Jeroen <gitlab.com@hendricksen.eu>
2022-06-28 14:26:28 +0200

    Require npm.token for release

--- release.sh
+++ release.sh
@@ -4,6 +4,11 @@
 # ToDo: test the new version in available from the README.md, ensuring
 # the change was documented.
 
+if [ -n "$1" ]; then
+    echo "You need to provide --npm.otp=xxxxxx"
+    exit 1
+fi
+
 # Test for any changes with regards to linting. If so, abort the release.
 npm run lint
 EXIT_CODE=$?
@@ -16,6 +21,6 @@ fi
 # 1. Make sure changes end up on the main branch on the server. Include an
 #    update to README.md as well.
 # 2. Run this script and choose the release # the same as done in README.md. Also add an otp token to authorized releasing it:
-# --npm.otp=000000 (or was it --otp=000000 ?)
+# --npm.otp=000000
 
 npx release-it --only-version "$@"
